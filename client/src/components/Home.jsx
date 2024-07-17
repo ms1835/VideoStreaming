f64dae05bdc2076cfab3049dd23cfc9f6f68bba9
ms1835 <mayanksingh5399@gmail.com>
2024-07-17 23:54:17 +0530

    Added page loader

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import VideoCard from './VideoCard';
+import Loader from './Loader';
 
 const Home = () => {
     const [videos, setVideos] = useState([]);
     const [reaction ,setReaction] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const getVideos = async() => {
             try{
+              setLoading(true);
                 const rawData = await fetch(`${import.meta.env.VITE_SERVER_URI}`, {
                     method: "GET",
                     credentials: 'include'
@@ -17,6 +20,8 @@ const Home = () => {
                 setVideos(response.data);
             } catch(error) {
                 console.log(error);
+            } finally {
+              setLoading(false);
             }
         }
         getVideos();
@@ -28,6 +33,7 @@ const Home = () => {
     }
 
   return (
+    loading ? <Loader /> :
     <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-8 m-8 md:m-16">
       {videos.map((video, index) => (
         <VideoCard key={index} video={video} handleReaction={handleReactionChange} />
